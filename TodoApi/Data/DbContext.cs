3c49968cea7e1b0e09384710592b0eb3b9748c63
Vo Tien Nam <votiennam2005@gmail.com>
2026-04-06 16:39:52 +0700

    hello 1

--- TodoApi/Data/DbContext.cs
+++ TodoApi/Data/DbContext.cs
@@ -5,6 +5,8 @@ using TodoApi.Models;
 public class TodoDbContext : DbContext {
     public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options) {}
     public DbSet<Todo> Todos { get; set; }
+
+    public int test() {}
 }
 
 // Chao Phat Nguyen
