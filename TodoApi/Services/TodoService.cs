1d2e0a245522427434d2fc91d3ccf2f525fbb9dd
Vo Tien Nam <votiennam2005@gmail.com>
2026-04-06 17:04:18 +0700

    feat(todo-serive): add task description field
    allow users to provide a description when creating a task
    
    Reviewed by Jane Doe
    
    Ref#67

--- TodoApi/Services/TodoService.cs
+++ TodoApi/Services/TodoService.cs
@@ -19,6 +19,7 @@ public class TodoService {
         if (string.IsNullOrEmpty(task.Title)) {
             throw new ArgumentException("Title is required");
         }
+        // abcxyz
         _todos.Add(task);
     }
 
